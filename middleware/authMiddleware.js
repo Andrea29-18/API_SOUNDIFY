0da7c470c25bfedcf9aafdbf208e0ddf451749db
Ale_Pucheta <aavp0316@gmail.com>
2024-06-11 13:08:21 -0600

    Esconder el JWT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const jwtSecret = '5f9b327ba659bad1da6609494f4a0157ae3e21e2f4ccd44cfcb42e8dbe3c226531738d'
+const jwtSecret = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.header('Authorization');
